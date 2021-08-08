2e8390bedcee85fc53a5c9d3ce4f04ec13390f26
Alison024 <Andreyfox.ru@gmail.com>
2021-08-08 17:32:04 +0300

    Added RewardErc20

---
+++ contracts/tokens/RewardErc20.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RewardErc20 is ERC20 {
+    constructor(uint256 supply) ERC20("Reward Token", "REWARDERC20") {
+        _mint(msg.sender, supply);
+    }
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+}
